8332a8ee813f060d4f3ab67ac967a05fe0c5e987
shivansh <shivanshmujoo@gmail.com>
2026-03-03 19:19:06 +0530

    versoni 1

---
+++ smth.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    std::cout<< "smth"<< std::endl;
+    std::cout<<"version-1"<<std::endl;
+
+    return 0;
+}
---
+++ tryinggit.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"tryinggit"<<std::endl;
+    std::cout<<"version-1"<<std::endl;
+
+    return 0;
+}
